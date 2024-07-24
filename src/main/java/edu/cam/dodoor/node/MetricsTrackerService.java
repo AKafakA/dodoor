2faff9fb69b30db996aa2dbd05ee254e3d00de1b
Wei Da <wd312@cl.cam.ac.uk>
2024-07-24 23:40:38 +0100

    use sigar to report metrics

--- src/main/java/edu/cam/dodoor/node/MetricsTrackerService.java
+++ src/main/java/edu/cam/dodoor/node/MetricsTrackerService.java
@@ -7,14 +7,13 @@ import org.apache.log4j.*;
 
 import java.io.File;
 import com.sun.management.OperatingSystemMXBean;
+import org.hyperic.sigar.*;
 import org.slf4j.LoggerFactory;
-import oshi.SystemInfo;
-import oshi.hardware.CentralProcessor;
-import oshi.hardware.HardwareAbstractionLayer;
 
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.util.concurrent.TimeUnit;
+
 
 public class MetricsTrackerService {
 
@@ -27,10 +26,6 @@ public class MetricsTrackerService {
     private final long _systemMemory;
     private long _timelineInSeconds;
     private final Slf4jReporter _slf4jReporter;
-
-    private final CentralProcessor _cpu;
-    private static final long SAMPLED_INTERVAL = 10;
-    private long[] _prevTicks;
 
 
     public MetricsTrackerService(int trackingInterval, Configuration config, NodeServiceMetrics nodeServiceMetrics) {
@@ -48,10 +43,6 @@ public class MetricsTrackerService {
             throw new RuntimeException(e);
         }
         _timelineInSeconds = 0;
-        SystemInfo _si = new SystemInfo();
-        HardwareAbstractionLayer _hal = _si.getHardware();
-        _cpu = _hal.getProcessor();
-        _prevTicks = _cpu.getSystemCpuLoadTicks();
 
         LOG = LoggerFactory.getLogger(MetricsTrackerService.class);
 
@@ -70,34 +61,28 @@ public class MetricsTrackerService {
                     Thread.sleep(TimeUnit.SECONDS.toMillis(_trackingInterval));
                     _timelineInSeconds += _trackingInterval;
                     logUsage();
-                } catch (InterruptedException e) {
+                } catch (InterruptedException | SigarException e) {
                     LOG.error("Metrics tracker thread interrupted", e);
                 }
             }
         }
     }
 
-    private void logUsage() throws InterruptedException {
-//        double cpuUsage = _operatingSystemMXBean.getCpuLoad();
-        long[] prevTicks = _cpu.getSystemCpuLoadTicks();
-        // 睡眠1s
-        TimeUnit.SECONDS.sleep(1);
-        long[] ticks = _cpu.getSystemCpuLoadTicks();
-        long nice = ticks[CentralProcessor.TickType.NICE.getIndex()] - prevTicks[CentralProcessor.TickType.NICE.getIndex()];
-        long irq = ticks[CentralProcessor.TickType.IRQ.getIndex()] - prevTicks[CentralProcessor.TickType.IRQ.getIndex()];
-        long softirq = ticks[CentralProcessor.TickType.SOFTIRQ.getIndex()] - prevTicks[CentralProcessor.TickType.SOFTIRQ.getIndex()];
-        long steal = ticks[CentralProcessor.TickType.STEAL.getIndex()] - prevTicks[CentralProcessor.TickType.STEAL.getIndex()];
-        long cSys = ticks[CentralProcessor.TickType.SYSTEM.getIndex()] - prevTicks[CentralProcessor.TickType.SYSTEM.getIndex()];
-        long user = ticks[CentralProcessor.TickType.USER.getIndex()] - prevTicks[CentralProcessor.TickType.USER.getIndex()];
-        long iowait = ticks[CentralProcessor.TickType.IOWAIT.getIndex()] - prevTicks[CentralProcessor.TickType.IOWAIT.getIndex()];
-        long idle = ticks[CentralProcessor.TickType.IDLE.getIndex()] - prevTicks[CentralProcessor.TickType.IDLE.getIndex()];
-        long totalCpu = user + nice + cSys + idle + iowait + irq + softirq + steal;
-        double cpuUsage = 1.0-(idle * 1.0 / totalCpu);
+    private void logUsage() throws InterruptedException, SigarException {
+        Sigar sigar = new Sigar();
+        CpuPerc cpu = sigar.getCpuPerc();
+        Mem mem = sigar.getMem();
+        double cpuUsage = _operatingSystemMXBean.getCpuLoad();
+        double cpuUsage2 = cpu.getCombined();
+        FileSystemUsage filesystemusage = sigar.getFileSystemUsage(_root.getPath());
         double memoryUsage =
                 (double) (_systemMemory - _operatingSystemMXBean.getFreeMemorySize()) / _systemMemory;
-        double freeSpace =  (double) _root.getFreeSpace() / _totalSpace;
-        LOG.info("Time(in Seconds): {} CPU usage: {} Memory usage: {} Disk usage: {}", new Object[]{_timelineInSeconds, cpuUsage, memoryUsage, freeSpace});
-        _prevTicks = _cpu.getSystemCpuLoadTicks();
+        double memoryUsage2 = mem.getUsedPercent();
+        double diskUsage2 = filesystemusage.getUsePercent();
+
+        double diskUsage =  (double) _root.getFreeSpace() / _totalSpace;
+        LOG.info("Time(in Seconds) OSM: {} CPU usage: {} Memory usage: {} Disk usage: {}", new Object[]{_timelineInSeconds, cpuUsage, memoryUsage, diskUsage});
+        LOG.info("Time(in Seconds) SIGAR : {} CPU usage: {} Memory usage: {} Disk usage: {}", new Object[]{_timelineInSeconds, cpuUsage2, memoryUsage2, diskUsage2});
     }
 
     public void start() {
