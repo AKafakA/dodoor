c164cd95f3b6f9d04c551df6829ccefbca520835
Wei Da <wd312@cl.cam.ac.uk>
2024-07-24 21:56:56 +0100

    add more azure data

--- deploy/python/data/generate_data.py
+++ deploy/python/data/generate_data.py
@@ -1,8 +1,8 @@
 from deploy.python.data.generator.azure.azure_data_generator import AzureDataGenerator
 
-data_path = "deploy/resources/data/azure_trace.sqlite"
+data_path = "deploy/resources/data/raw_data/azure_trace.sqlite"
 output_path = "deploy/resources/data/azure_data"
 
 azure_data_generator = AzureDataGenerator(data_path, output_path, machine_ids=range(0, 10))
-data = azure_data_generator.generate(1000000, 0, 60000, [0, 0.1])
+data = azure_data_generator.generate(100000, 0, 60000, [0, 14])
 azure_data_generator.write_data(data)
