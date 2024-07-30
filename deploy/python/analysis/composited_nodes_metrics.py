0fa111174603ae7c703e100b7c32353f63333ddc
Wei Da <wd312@cl.cam.ac.uk>
2024-07-30 20:30:41 +0100

    add python analysis functions

---
+++ deploy/python/analysis/composited_nodes_metrics.py
@@ -0,0 +1,112 @@
+import os
+from typing import List
+
+import numpy as np
+
+from deploy.python.analysis.node_metrics import NodeMetrics
+
+
+def get_nodes_metrics_lists(metrics_name, metrics_length, nodes: List[NodeMetrics]):
+    nodes_metrics = []
+    for i in range(metrics_length):
+        nodes_metric = []
+        for node in nodes:
+            nodes_metric.append(node.metrics[metrics_name][i])
+        nodes_metrics.append(nodes_metric)
+    return nodes_metrics
+
+
+def get_average_nodes_metrics(metrics_name, metrics_length, nodes: List[NodeMetrics]):
+    nodes_metrics_lists = get_nodes_metrics_lists(metrics_name, metrics_length, nodes)
+    average_nodes_metrics = [sum(nodes_metrics) / len(nodes) for nodes_metrics in nodes_metrics_lists]
+    return average_nodes_metrics
+
+
+def get_max_nodes_metrics(metrics_name, metrics_length, nodes: List[NodeMetrics]):
+    nodes_metrics_lists = get_nodes_metrics_lists(metrics_name, metrics_length, nodes)
+    max_nodes_metrics = [max(nodes_metrics) for nodes_metrics in nodes_metrics_lists]
+    return max_nodes_metrics
+
+
+def get_total_nodes_metrics(metrics_name, metrics_length, nodes: List[NodeMetrics]):
+    nodes_metrics_lists = get_nodes_metrics_lists(metrics_name, metrics_length, nodes)
+    total_nodes_metrics = [sum(nodes_metrics) for nodes_metrics in nodes_metrics_lists]
+    return total_nodes_metrics
+
+
+def get_variance_nodes_metrics(metrics_name, metrics_length, nodes: List[NodeMetrics]):
+    nodes_metrics_lists = get_nodes_metrics_lists(metrics_name, metrics_length, nodes)
+    variance_nodes_metrics = [np.var(np.array(nodes_metrics)) for nodes_metrics in nodes_metrics_lists]
+    return variance_nodes_metrics
+
+
+class CompositedNodesMetrics:
+    def __init__(self, node_log_dir, id="default"):
+        self.id = id
+        self.node_metrics = []
+        self.min_start_step = 100000
+        self.max_length = 0
+        for filename in os.listdir(os.path.join(os.getcwd(), node_log_dir)):
+            if filename.endswith(".log"):
+                node_id = int(filename.split("_")[-1].split(".")[0])
+                log_file_path = os.path.join(os.getcwd(), node_log_dir, filename)
+                node_metric = NodeMetrics(log_file_path, node_id)
+                node_metric.parse()
+                self.node_metrics.append(node_metric)
+                self.min_start_step = min(node_metric.start_step, self.min_start_step)
+                self.max_length = max(node_metric.length, self.max_length)
+
+        for node_metric in self.node_metrics:
+            node_metric.calibrate(self.min_start_step, self.max_length)
+        self.length = self.max_length - self.min_start_step
+
+    def get_num_nodes(self):
+        return len(self.node_metrics)
+
+    def get_total_messages(self):
+        return get_total_nodes_metrics("message", self.length, self.node_metrics)
+
+    def get_total_waiting_tasks(self):
+        return get_total_nodes_metrics("num_waiting_tasks", self.length, self.node_metrics)
+
+    def get_max_waiting_tasks(self):
+        return get_max_nodes_metrics("num_waiting_tasks", self.length, self.node_metrics)
+
+    def get_average_waiting_tasks(self):
+        return get_average_nodes_metrics("num_waiting_tasks", self.length, self.node_metrics)
+
+    def get_average_task_rate_average(self):
+        return get_average_nodes_metrics("task_rate_mean", self.length, self.node_metrics)
+
+    def get_average_task_waited_duration(self):
+        total_waited_duration = get_total_nodes_metrics("duration_task_waited_total", self.length, self.node_metrics)
+        total_waited_tasks = get_total_nodes_metrics("count_tasks_waited", self.length, self.node_metrics)
+        return [total_waited_duration[i] / total_waited_tasks[i]
+                if total_waited_tasks[i] != 0 else 0 for i in range(self.length)]
+
+    def get_total_finished_tasks(self):
+        return get_total_nodes_metrics("num_finished_tasks", self.length, self.node_metrics)
+
+    def get_cpu_usage_mean_and_variance(self):
+        return get_average_nodes_metrics("cpu_usage", self.length, self.node_metrics), \
+            get_variance_nodes_metrics("cpu_usage", self.length, self.node_metrics)
+
+    def get_mem_usage_mean_and_variance(self):
+        return get_average_nodes_metrics("mem_usage", self.length, self.node_metrics), \
+            get_variance_nodes_metrics("mem_usage", self.length, self.node_metrics)
+
+    def get_disk_usage_mean_and_variance(self):
+        return get_average_nodes_metrics("disk_usage", self.length, self.node_metrics), \
+            get_variance_nodes_metrics("disk_usage", self.length, self.node_metrics)
+
+
+if __name__ == "__main__":
+    test_node_log_dir = "deploy/resources/log/node/test/dodoor_node_metrics_103.log"
+    node_metrics = NodeMetrics(test_node_log_dir, "1")
+    test_node_log_dir = "deploy/resources/log/node/test"
+    composited_nodes_metrics = CompositedNodesMetrics(test_node_log_dir)
+    print(composited_nodes_metrics.get_num_nodes())
+    print(composited_nodes_metrics.get_cpu_usage_mean_and_variance())
+    print(composited_nodes_metrics.get_average_waiting_tasks())
+    print(composited_nodes_metrics.get_total_waiting_tasks())
+    print(composited_nodes_metrics.get_average_task_waited_duration())
---
+++ deploy/python/analysis/scheduler_metrics.py
@@ -0,0 +1,72 @@
+import re
+
+
+class SchedulerMetrics:
+    message_counter_pattern = re.compile("type=COUNTER, name=scheduler.metrics.num.messages, count=\d+")
+    e2e_latency_pattern = re.compile("type=HISTOGRAM, name=scheduler.metrics.tasks.e2e.scheduling.latency.histograms, "
+                                     "count=\d+, min=\d+, "
+                                     "max=\d+, "
+                                     "mean=\d+.\d+, "
+                                     "stddev=\d+.\d+, "
+                                     "p50=\d+.\d+, "
+                                     "p75=\d+.\d+, "
+                                     "p95=\d+.\d+, "
+                                     "p98=\d+.\d+, "
+                                     "p99=\d+.\d+, "
+                                     "p999=\d+.\d+")
+    task_rate_pattern = re.compile("type=METER, name=scheduler.metrics.tasks.rate, count=\d+, "
+                                   "m1_rate=\d+.\d+, m5_rate=\d+.\d+, m15_rate=\d+.\d+, mean_rate=\d+.\d+, "
+                                   "rate_unit=events/second")
+
+    def __init__(self, log_file):
+        self.metrics = {"num_messages": [],
+                        "e2e_latency_avg": [],
+                        "e2e_latency_max": [],
+                        "e2e_latency_min": [],
+                        "e2e_latency_std": [],
+                        "e2e_latency_p50": [],
+                        "e2e_latency_p99": [],
+                        "task_rate_mean": []}
+        self.log_file = log_file
+
+    def parse(self):
+        with open(self.log_file, 'r') as f:
+            for line in f.readlines():
+                if self.message_counter_pattern.match(line):
+                    self.metrics["num_messages"].append(int(line.split(",")[2].split("=")[1]))
+                elif self.task_rate_pattern.match(line):
+                    self.metrics["task_rate_mean"].append(float(line.split(",")[6].split("=")[1]))
+                elif self.e2e_latency_pattern.match(line):
+                    self.metrics["e2e_latency_avg"].append(float(line.split(",")[5].split("=")[1]))
+                    self.metrics["e2e_latency_max"].append(float(line.split(",")[4].split("=")[1]))
+                    self.metrics["e2e_latency_min"].append(float(line.split(",")[3].split("=")[1]))
+                    self.metrics["e2e_latency_std"].append(float(line.split(",")[6].split("=")[1]))
+                    self.metrics["e2e_latency_p50"].append(float(line.split(",")[7].split("=")[1]))
+                    self.metrics["e2e_latency_p99"].append(float(line.split(",")[11].split("=")[1]))
+        return self.metrics
+
+    def get_num_messages(self):
+        return self.metrics["num_messages"]
+
+    def get_task_rate_mean(self):
+        return self.metrics["task_rate_mean"]
+
+    def get_e2e_latency_avg(self):
+        return self.metrics["e2e_latency_avg"]
+
+    def get_e2e_latency_max(self):
+        return self.metrics["e2e_latency_max"]
+
+    def get_e2e_latency_p99(self):
+        return self.metrics["e2e_latency_p99"]
+
+    def get_e2e_latency_p50(self):
+        return self.metrics["e2e_latency_p50"]
+
+
+if __name__ == "__main__":
+    test_scheduler_log = "deploy/resources/log/scheduler/test/dodoor_scheduler_metrics_102.log"
+    test_scheduler_metrics = SchedulerMetrics(test_scheduler_log)
+    test_scheduler_metrics.parse()
+    for key, value in test_scheduler_metrics.metrics.items():
+        print(key, len(value))
