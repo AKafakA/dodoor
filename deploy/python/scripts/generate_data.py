97e7b96eccca8f0b150cd0976842e1534098b404
Wei Da <wd312@cl.cam.ac.uk>
2024-08-12 20:09:32 +0100

    add cloudlab specifical data

--- deploy/python/scripts/generate_data.py
+++ deploy/python/scripts/generate_data.py
@@ -1,10 +1,10 @@
 from deploy.python.data.generator.azure.azure_data_generator import AzureDataGenerator
 
 data_path = "deploy/resources/data/raw_data/azure_trace.sqlite"
-output_path = "deploy/resources/data/azure_data"
+output_path = "deploy/resources/data/azure_data_cloudlab"
 
 azure_data_generator = AzureDataGenerator(data_path, output_path, machine_ids=range(1, 2), max_cores=24,
                                           max_memory=256 * 1024)
 data = azure_data_generator.generate(10000000, 0, 1000 * 60 * 10, [0, 1/12], time_shift=-1,
-                                     max_cores=6, max_memory=60 * 1024, max_disk=-1)
+                                     max_cores=8, max_memory=60 * 1024, max_disk=-1)
 azure_data_generator.write_data(data)
