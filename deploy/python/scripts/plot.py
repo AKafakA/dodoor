3b2c3735eddaa8d247ff033a988f4816335666b5
Wei Da <wd312@cl.cam.ac.uk>
2024-08-22 15:56:37 +0100

    adjust the name of line in plot

--- deploy/python/scripts/plot.py
+++ deploy/python/scripts/plot.py
@@ -17,7 +17,7 @@ if not os.path.exists(target_dir):
     os.makedirs(target_dir)
 
 time_steps = 10
-max_checkpoints = 1100
+max_checkpoints = 1000
 composited_node_host_dir = "deploy/resources/log/node/{}".format(experiment_name)
 
 var_resource_mean_lists = {}
@@ -25,6 +25,9 @@ average_resource_mean_lists = {}
 max_num_waiting_task_lists = {}
 average_num_waiting_task_lists = {}
 variance_waiting_task_lists = {}
+
+test_scheduler_type = ["sparrow", "dodoor"]
+test_cpu_weight = [10.0, 1.0]
 
 for scheduler_name in os.listdir(composited_node_host_dir):
     node_file = os.path.join(composited_node_host_dir, scheduler_name)
@@ -34,7 +37,13 @@ for scheduler_name in os.listdir(composited_node_host_dir):
     average_num_waiting_tasks = nodes_metrics.get_average_waiting_tasks()
     variance_waiting_tasks = nodes_metrics.get_variance_waiting_tasks()
 
-    scheduler_name = scheduler_name.split("_")[0]
+    scheduler_type = scheduler_name.split("_")[0]
+    cpu = scheduler_name.split("_")[-3]
+    scheduler_name = "{}_{}".format(scheduler_type, cpu)
+
+    if scheduler_type not in test_scheduler_type or float(cpu) not in test_cpu_weight:
+        continue
+
     average_num_waiting_task_lists[scheduler_name] = average_num_waiting_tasks
     max_num_waiting_task_lists[scheduler_name] = max_num_waiting_tasks
     var_resource_mean_lists[scheduler_name] = var_resource_mean
@@ -63,48 +72,51 @@ for scheduler_name in os.listdir(composited_node_host_dir):
     plt.plot(average_cpu_usage[:max_checkpoints], label=scheduler_name)
 
 plt.figure(1)
-plt.legend(loc='upper right', handlelength=1, frameon=False)
+plt.legend(loc='best', handlelength=1, frameon=False)
 plt.xlabel("{} seconds".format(time_steps))
 plt.ylabel("average resources usage variance")
 plt.savefig("{}/average_usage_variance.png".format(target_dir))
 
 plt.figure(2)
-plt.legend(loc='upper right', handlelength=1, frameon=False)
+plt.legend(loc='best', handlelength=1, frameon=False)
 plt.xlabel("{} seconds".format(time_steps))
 plt.ylabel("average resources usage mean")
 plt.savefig("{}/average_resource_usage_mean.png".format(target_dir))
 
 plt.figure(3)
-plt.legend(loc='upper right', handlelength=1, frameon=False)
+plt.legend(loc='best', handlelength=1, frameon=False)
 plt.xlabel("{} seconds".format(time_steps))
 plt.ylabel("waiting tasks difference")
 plt.savefig("{}/waiting_tasks_max_average_difference.png".format(target_dir))
 
 plt.figure(4)
-plt.legend(loc='upper right', handlelength=1, frameon=False)
+plt.legend(loc='best', handlelength=1, frameon=False)
 plt.xlabel("{} seconds".format(time_steps))
 plt.ylabel("waiting tasks variance")
 plt.savefig("{}/waiting_tasks_variance.png".format(target_dir))
 
 plt.figure(5)
-plt.legend(loc='upper right', handlelength=1, frameon=False)
+plt.legend(loc='best', handlelength=1, frameon=False)
 plt.xlabel("{} seconds".format(time_steps))
 plt.ylabel("average waiting time")
 plt.savefig("{}/average_waiting_time.png".format(target_dir))
 
 plt.figure(10)
-plt.legend(loc='upper right', handlelength=1, frameon=False)
+plt.legend(loc='best', handlelength=1, frameon=False)
 plt.xlabel("{} seconds".format(time_steps))
 plt.ylabel("average cpu usage")
 plt.savefig("{}/average_cpu_usage.png".format(target_dir))
-
 
 scheduler_host_dir = "deploy/resources/log/scheduler/{}".format(experiment_name)
 for scheduler_name in os.listdir(scheduler_host_dir):
     for scheduler_log_file in os.listdir(os.path.join(scheduler_host_dir, scheduler_name)):
         if "scheduler_metrics" in scheduler_log_file:
             scheduler_metrics = SchedulerMetrics(os.path.join(scheduler_host_dir, scheduler_name, scheduler_log_file))
-            scheduler_name = scheduler_name.split("_")[0]
+            scheduler_type = scheduler_name.split("_")[0]
+            cpu = scheduler_name.split("_")[-3]
+            scheduler_name = "{}_{}".format(scheduler_type, cpu)
+            if scheduler_type not in test_scheduler_type or float(cpu) not in test_cpu_weight:
+                continue
 
             num_messages = scheduler_metrics.get_num_messages()
             task_rate_m1 = scheduler_metrics.get_task_rate_m1()
@@ -125,13 +137,13 @@ for scheduler_name in os.listdir(scheduler_host_dir):
                      label=scheduler_name)
 
 plt.figure(6)
-plt.legend(loc='upper right', handlelength=1, frameon=False)
+plt.legend(loc='best', handlelength=1, frameon=False)
 plt.xlabel("{} seconds".format(time_steps))
 plt.ylabel("num messages")
 plt.savefig("{}/num_messages.png".format(target_dir))
 
 plt.figure(7)
-plt.legend(loc='upper right', handlelength=1, frameon=False)
+plt.legend(loc='best', handlelength=1, frameon=False)
 plt.xlabel("{} seconds".format(time_steps))
 plt.ylabel("task rate m1")
 plt.savefig("{}/task_rate_m1.png".format(target_dir))
