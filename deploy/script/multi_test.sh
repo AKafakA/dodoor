46a7a912870eb04ac70e2c389c853565fc895635
Wei Da <wd312@cl.cam.ac.uk>
2024-09-09 18:13:03 +0100

    change muti test

--- deploy/script/multi_test.sh
+++ deploy/script/multi_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SLEEP_TIME="15000"
+SLEEP_TIME="14400"
 BETA=1.0
 BATCH_SIZE=100
 CPU_WEIGHT=10.0
@@ -14,7 +14,7 @@ export PYTHONPATH=$PYTHONPATH:~/Code/scheduling/dodoor
 parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host  -i "rm ~/*.log && rm ~/*.out"
 rm -rf deploy/resources/log/*
 
-for i in "sparrow" "asyncSparrow" "random" "dodoor" "cachedSparrow" "prequal";
+for i in "sparrow" "prequal" "random" "dodoor" "cachedSparrow";
   do
     echo "run the exp for $i"
     sh deploy/script/single_test_cloudlab.sh $i $BATCH_SIZE $SLOT_SIZE $BETA $CPU_WEIGHT $DATA_PATH $DURATION_WEIGHT $NUM_SCHEDULER
@@ -22,49 +22,49 @@ for i in "sparrow" "asyncSparrow" "random" "dodoor" "cachedSparrow" "prequal";
     python3 deploy/python/scripts/collect_logs.py $i $BATCH_SIZE $SLOT_SIZE $BETA $CPU_WEIGHT $DURATION_WEIGHT $NUM_SCHEDULER
 done
 
-parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host  -i "rm ~/*.log && rm ~/*.out"
-SLOT_SIZE=3
-NUM_SCHEDULER=5
-for i in "sparrow" "asyncSparrow" "random" "dodoor" "cachedSparrow" "prequal";
-  do
-    echo "run the exp for $i"
-    sh deploy/script/single_test_cloudlab.sh $i $BATCH_SIZE 3 $BETA $CPU_WEIGHT $DATA_PATH $DURATION_WEIGHT $NUM_SCHEDULER
-    sleep ${SLEEP_TIME}
-    python3 deploy/python/scripts/collect_logs.py $i $BATCH_SIZE 3 $BETA $CPU_WEIGHT $DURATION_WEIGHT $NUM_SCHEDULER
-done
-
-parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host  -i "rm ~/*.log && rm ~/*.out"
-SLOT_SIZE=4
-NUM_SCHEDULER=10
-for i in "sparrow" "asyncSparrow" "random" "dodoor" "cachedSparrow" "prequal";
-  do
-    echo "run the exp for $i"
-    sh deploy/script/single_test_cloudlab.sh $i $BATCH_SIZE 3 $BETA $CPU_WEIGHT $DATA_PATH $DURATION_WEIGHT $NUM_SCHEDULER
-    sleep ${SLEEP_TIME}
-    python3 deploy/python/scripts/collect_logs.py $i $BATCH_SIZE 3 $BETA $CPU_WEIGHT $DURATION_WEIGHT $NUM_SCHEDULER
-done
-
-parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host  -i "rm ~/*.log && rm ~/*.out"
-SLOT_SIZE=4
-NUM_SCHEDULER=5
-for i in "dodoor";
-  do
-  # shellcheck disable=SC2043
-  for slot_size in 4
-    do
-    for batch_size in 100
-      do
-      for cpu_weight in 10.0
-        do
-          for duration_weight in 0.55 0.6
-            do
-              echo "run the exp for $i with cpu weight $cpu_weight and duration weight $duration_weight"
-              sh deploy/script/single_test_cloudlab.sh $i $batch_size $slot_size $BETA $cpu_weight $DATA_PATH $duration_weight $NUM_SCHEDULER
-              sleep ${SLEEP_TIME}
-              python3 deploy/python/scripts/collect_logs.py $i $batch_size $slot_size $BETA $cpu_weight $duration_weight $NUM_SCHEDULER
-              parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host -i "sudo chmod -R 777 /users/asdwb/dodoor && mkdir -p ~/dodoor/deploy/resources/log/$i-$batch_size-$slot_size-$BETA-$cpu_weight-$duration_weight && mv ~/*.log ~/dodoor/deploy/resources/log/$i-$batch_size-$slot_size-$BETA-$cpu_weight-$duration_weight/. & mv ~/*.out ~/dodoor/deploy/resources/log/$i-$batch_size-$slot_size-$BETA-$cpu_weight-$duration_weight/."
-            done
-        done
-      done
-    done
-done
+#parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host  -i "rm ~/*.log && rm ~/*.out"
+#SLOT_SIZE=3
+#NUM_SCHEDULER=5
+#for i in "sparrow" "prequal" "random" "dodoor" "cachedSparrow";
+#  do
+#    echo "run the exp for $i"
+#    sh deploy/script/single_test_cloudlab.sh $i $BATCH_SIZE 3 $BETA $CPU_WEIGHT $DATA_PATH $DURATION_WEIGHT $NUM_SCHEDULER
+#    sleep ${SLEEP_TIME}
+#    python3 deploy/python/scripts/collect_logs.py $i $BATCH_SIZE 3 $BETA $CPU_WEIGHT $DURATION_WEIGHT $NUM_SCHEDULER
+#done
+#
+#parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host  -i "rm ~/*.log && rm ~/*.out"
+#SLOT_SIZE=4
+#NUM_SCHEDULER=10
+#for i in "sparrow" "prequal" "random" "dodoor" "cachedSparrow";
+#  do
+#    echo "run the exp for $i"
+#    sh deploy/script/single_test_cloudlab.sh $i $BATCH_SIZE 3 $BETA $CPU_WEIGHT $DATA_PATH $DURATION_WEIGHT $NUM_SCHEDULER
+#    sleep ${SLEEP_TIME}
+#    python3 deploy/python/scripts/collect_logs.py $i $BATCH_SIZE 3 $BETA $CPU_WEIGHT $DURATION_WEIGHT $NUM_SCHEDULER
+#done
+#
+#parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host  -i "rm ~/*.log && rm ~/*.out"
+#SLOT_SIZE=4
+#NUM_SCHEDULER=5
+#for i in "dodoor";
+#  do
+#  # shellcheck disable=SC2043
+#  for slot_size in 4
+#    do
+#    for batch_size in 100
+#      do
+#      for cpu_weight in 10.0
+#        do
+#          for duration_weight in 0.55 0.6
+#            do
+#              echo "run the exp for $i with cpu weight $cpu_weight and duration weight $duration_weight"
+#              sh deploy/script/single_test_cloudlab.sh $i $batch_size $slot_size $BETA $cpu_weight $DATA_PATH $duration_weight $NUM_SCHEDULER
+#              sleep ${SLEEP_TIME}
+#              python3 deploy/python/scripts/collect_logs.py $i $batch_size $slot_size $BETA $cpu_weight $duration_weight $NUM_SCHEDULER
+#              parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host -i "sudo chmod -R 777 /users/asdwb/dodoor && mkdir -p ~/dodoor/deploy/resources/log/$i-$batch_size-$slot_size-$BETA-$cpu_weight-$duration_weight && mv ~/*.log ~/dodoor/deploy/resources/log/$i-$batch_size-$slot_size-$BETA-$cpu_weight-$duration_weight/. & mv ~/*.out ~/dodoor/deploy/resources/log/$i-$batch_size-$slot_size-$BETA-$cpu_weight-$duration_weight/."
+#            done
+#        done
+#      done
+#    done
+#done
