a0fe5a7c65152071ffbb511907330b16f326a30a
Wei Da <wd312@cl.cam.ac.uk>
2024-09-24 18:34:50 +0100

    change the test scripts for serverless

--- deploy/script/caelum_test_for_severless.sh
+++ deploy/script/caelum_test_for_severless.sh
@@ -24,13 +24,13 @@ DURATION_WEIGHT=0.5
 
 parallel-ssh -h deploy/resources/host_addresses/caelum/small_caelum  -i "rm ~/*.log && rm ~/*.out"
 
-parallel-ssh -h deploy/resources/host_addresses/caelum/small_caelum -i "cd dodoor && git fetch --all && git add . && git stash && git checkout sparrow_impl && git reset --hard HEAD~10 && git pull && sh rebuild.sh"
+parallel-ssh -h deploy/resources/host_addresses/caelum/small_caelum -i "cd dodoor && git fetch --all && git add . && git stash && git checkout test_serverless && git pull && sh rebuild.sh"
 
 parallel-ssh -h deploy/resources/host_addresses/caelum/small_caelum -i "pkill -f dodoor"
 
 parallel-ssh -h deploy/resources/host_addresses/caelum/small_caelum  -i "pkill -f stress"
 
-parallel-ssh -h deploy/resources/host_addresses/caelum/small_caelum -i  "cd dodoor && sudo python3 deploy/python/scripts/config_generator.py -d deploy/resources/host_addresses/caelum/caelum_scheduler_ip -n deploy/resources/host_addresses/caelum/small_caelum_host_ip  -s deploy/resources/host_addresses/caelum/caelum_scheduler_ip --replay_with_delay False --scheduler-ports $SCHEDULER_PORTS --use-configable-address True --batch-size $BATCH_SIZE --beta $BETA --cores $CORES --memory $MEMORY --scheduler-type $SCHEDULER_TYPE --scheduler-num-tasks-update $SCHEDULER_NUM_TASKS_UPDATE --num-slots $SLOT_SIZE --network_interface $NETWORK_INTERFACE --cpu_weight $CPU_WEIGHT --duration_weight $DURATION_WEIGHT --task_duration_unit ns"
+parallel-ssh -h deploy/resources/host_addresses/caelum/small_caelum -i  "cd dodoor && sudo python3 deploy/python/scripts/config_generator.py -d deploy/resources/host_addresses/caelum/caelum_scheduler_ip -n deploy/resources/host_addresses/caelum/small_caelum_host_ip  -s deploy/resources/host_addresses/caelum/caelum_scheduler_ip --replay_with_delay False --scheduler-ports $SCHEDULER_PORTS --use-configable-address True --batch-size $BATCH_SIZE --beta $BETA --cores $CORES --memory $MEMORY --scheduler-type $SCHEDULER_TYPE --scheduler-num-tasks-update $SCHEDULER_NUM_TASKS_UPDATE --num-slots $SLOT_SIZE --network_interface $NETWORK_INTERFACE --cpu_weight $CPU_WEIGHT --duration_weight $DURATION_WEIGHT --task_duration_unit ms"
 #
 parallel-ssh -t 0 -h deploy/resources/host_addresses/caelum/small_node -i "nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar edu.cam.dodoor.ServiceDaemon -c ~/dodoor/config.conf -d false -s false -n true  1>service.out  2>/dev/null &"
 #
