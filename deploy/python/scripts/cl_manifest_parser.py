f423d0e9af71ed6f72a06c0d63513201639890f4
Wei Da <wd312@cl.cam.ac.uk>
2025-07-02 17:49:32 +0100

    change the config generator

--- deploy/python/scripts/cl_manifest_parser.py
+++ deploy/python/scripts/cl_manifest_parser.py
@@ -1,14 +1,15 @@
 import os
-from xml.dom.minidom import parse
+import json
 import xml.etree.ElementTree as ET
 from collections import OrderedDict
 import subprocess
 
-manifest_path = "deploy/resources/cl_manifest.xml"
-output_path = "deploy/resources/host_addresses/cloud_lab"
+manifest_path = "deploy/resources/configuration/manifest.xml"
+template_json_path = "deploy/resources/configuration/host_config_template.json"
+host_ssh_path = "deploy/resources/host_addresses/cloud_lab"
 
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
+if not os.path.exists(host_ssh_path):
+    os.makedirs(host_ssh_path)
 
 tree = ET.parse(manifest_path)
 num_schedulers = 1
@@ -16,6 +17,7 @@ num_schedulers = 1
 nodes = {}
 root = tree.getroot()
 upload = True
+
 
 for child in root:
     if "node" in child.tag:
@@ -29,16 +31,22 @@ for child in root:
             if "services" in subchild.tag:
                 host_name = subchild[0].get("hostname")
                 node_info["hostname"] = host_name
+            if "hardware_type" in subchild.tag:
+                hardware_type = subchild.get("name")
+                node_info["hardware_type"] = hardware_type
 
 nodes = OrderedDict(sorted(nodes.items()))
 
-host_files = output_path + "/test_host"
-scheduler_files = output_path + "/test_scheduler"
-node_files = output_path + "/test_nodes"
-node_ip = output_path + "/test_node_ip"
-scheduler_ip = output_path + "/test_scheduler_ip"
+host_files = host_ssh_path + "/test_host"
+scheduler_files = host_ssh_path + "/test_scheduler"
+node_files = host_ssh_path + "/test_nodes"
+node_ip = host_ssh_path + "/test_node_ip"
+scheduler_ip = host_ssh_path + "/test_scheduler_ip"
+
+host_config_json = host_ssh_path + "/host_config.json"
 
 host_names = []
+host_json = json.load(open(template_json_path, "r"))
 with open(host_files, "w+") as f, open(scheduler_files, "w+") as s, open(node_files, "w+") as n, \
         open(node_ip, "w+") as nip, open(scheduler_ip, "w+") as sip:
     j = 0
@@ -53,10 +61,21 @@ with open(host_files, "w+") as f, open(scheduler_files, "w+") as s, open(node_fi
             n.write("asdwb@" + node_info["hostname"] + "\n")
             nip.write(node + ":" + node_info["ip_adresses"] + "\n")
         host_names.append("asdwb@" + node_info["hostname"])
+        if node_info["hardware_type"] == host_json["scheduler"]["type"]:
+            host_json["scheduler"]["hosts"].append(node_info["ip_adresses"])
+            host_json["datastore"]["hosts"].append(node_info["ip_adresses"])
+        else:
+            for node_type in host_json["nodes"]["node.types"]:
+                if node_type["node.type"] == node_info["hardware_type"]:
+                    node_type["hosts"].append(node_info["ip_adresses"])
+                    break
+
+json.dump(host_json, open(host_config_json, "w+"), indent=4)
+
 
 if upload:
     for host in host_names:
-        command = "scp -r %s %s:%s" % (output_path, host, "~/")
+        command = "scp -r %s %s:%s" % (host_ssh_path, host, "~/")
         subprocess.call(command, shell=True)
         print("{} host information updated.".format(host))
 
--- deploy/python/scripts/config_generator.py
+++ deploy/python/scripts/config_generator.py
@@ -6,36 +6,10 @@ def parse_args():
                                 "\n\n generate the configuration file for dodoor experiments")
     parser.add_option("--scheduler-type", default="dodoor",
                       help="The type of scheduler to be used in the experiment")
-    parser.add_option("--use-configable-address", default=False,
-                      help="Whether to use the configable address for the services")
-    parser.add_option("-n", "--nodes-file",
-                      help="Inject the host ip addresses of nodes into the config file from the host files")
-    parser.add_option("-s", "--scheduler-file",
-                      help="Inject the scheduler ip address into the config file from the scheduler file")
-    parser.add_option("-d", "--data-store-file",
-                      help="Inject the data store ip address into the config file from the data store file")
-    parser.add_option("--num-schedulers", default=1,
-                      help="The number of schedulers in the system")
-    parser.add_option("--num-nodes", default=100,
-                      help="The number of nodes in the system")
-    parser.add_option("--num-data-stores", default=1,
-                      help="The number of data stores in the system")
     parser.add_option("--scheduler-data-store-colocated", default=True,
                       help="Whether the scheduler and data store are colocated on the same host")
     parser.add_option("-o", "--output", default="./config.conf",
                       help="The output path of generated configuration file")
-    parser.add_option("--node-monitor-ports", default="20501",
-                      help="The port numbers of the node, passing multiple options separated by comma. Different ports "
-                           "will point to a singleton node monitor instances")
-    parser.add_option("--node-enqueue-ports", default="20502",
-                      help="The port number of the internal service to enqueue and dequeue the tasks")
-    parser.add_option("--data-store-ports", default="20503",
-                      help="The port number of the data store, passing multiple options separated by comma, "
-                           "same as scheduler ports to create multiple data stores instances")
-    parser.add_option("--scheduler-ports", default="20504",
-                      help="The port numbers of the scheduler, passing multiple options separated by comma. "
-                           "Each port will be used by a individual scheduler. So, the number of ports should be equal "
-                           "to the number of schedulers per host.")
     parser.add_option("--scheduler-thrift-threads", default=8,
                       help="The number of threads running in scheduler service to listen to the thrift requests")
     parser.add_option("--node-monitor-thrift-threads", default=1,
@@ -54,16 +28,6 @@ def parse_args():
                       help="The trace file of scheduler service to be used in the experiment")
     parser.add_option("--tracking-interval", default=10,
                       help="The interval in seconds of tracking the system status")
-    parser.add_option("--cores", default=6,
-                      help="The number of available cores in the system to run the tasks")
-    parser.add_option("--memory", default=30720,
-                      help="The amount of memory in Mb in the system to run the tasks")
-    parser.add_option("--disk", default=307200,
-                      help="The amount of disk in Mb in the system to run the tasks")
-    parser.add_option("--num-slots", default=4,
-                      help="The number of slots in each node to run the tasks in parallel. So, "
-                           "the maximal resources can be used by the tasks like cores will be cores / num_slots."
-                           "And the large tasks exceeding the resources will be rejected.")
     parser.add_option("--beta", default=0.85,
                       help="The beta value used for 1 + beta process for random scheduling")
     parser.add_option("--batch-size", default=82,
@@ -116,53 +80,6 @@ def main():
     config_path = options.output
     file = open(config_path, "w")
 
-    if options.use_configable_address:
-        with open(options.nodes_file, "r") as f:
-            write_ip_port(file, f.readlines(), "static.node")
-
-        with open(options.scheduler_file, "r") as f:
-            write_ip_port(file, f.readlines(), "static.scheduler")
-
-        with open(options.data_store_file, "r") as f:
-            write_ip_port(file, f.readlines(), "static.datastore")
-    else:
-        num_scheduler = int(options.num_schedulers)
-        num_nodes = int(options.num_nodes)
-        num_data_stores = int(options.num_data_stores)
-        scheduler_data_store_colocated = options.scheduler_data_store_colocated
-        start_ip_prefix = "10.10.1."
-        j = 0
-        if scheduler_data_store_colocated:
-            assert num_scheduler == num_data_stores, "The number of schedulers and data stores should be the same"
-            address = ""
-            for i in range(num_scheduler):
-                address += start_ip_prefix + str(j + 1) + ","
-                j += 1
-            address = address[:-1]
-            file.write("static.scheduler = " + address + "\n")
-            file.write("static.datastore = " + address + "\n")
-        else:
-            address = ""
-            for i in range(num_scheduler):
-                address += start_ip_prefix + str(j + 1) + ","
-                j += 1
-            address = address[:-1]
-            file.write("static.scheduler = " + address + "\n")
-
-            address = ""
-            for i in range(j, j + num_data_stores):
-                address += start_ip_prefix + str(j + 1) + ","
-                j += 1
-            address = address[:-1]
-            file.write("static.datastore = " + address + "\n")
-
-        address = ""
-        for i in range(j, j + num_nodes):
-            address += start_ip_prefix + str(j + 1) + ","
-            j += 1
-        address = address[:-1]
-        file.write("static.node = " + address + "\n")
-
     scheduler_type = options.scheduler_type
     file.write("scheduler.type = {} \n".format(scheduler_type))
 
@@ -173,25 +90,15 @@ def main():
         file.write("datastore.metrics.log.file = {} \n".format(options.datastore_trace_file))
         file.write("scheduler.metrics.log.file = {} \n".format(options.scheduler_trace_file))
 
-    file.write("system.cores = {} \n".format(options.cores))
-
-    file.write("system.memory = {} \n".format(options.memory))
-    file.write("system.disk = {} \n".format(options.disk))
-    file.write("node_monitor.num_slots = {} \n".format(options.num_slots))
-
     file.write("dodoor.beta = {} \n".format(options.beta))
     file.write("dodoor.batch_size = {} \n".format(options.batch_size))
 
-    file.write("scheduler.thrift.ports = {} \n".format(options.scheduler_ports))
     file.write("scheduler.thrift.threads = {} \n".format(options.scheduler_thrift_threads))
 
-    file.write("datastore.thrift.ports = {} \n".format(options.data_store_ports))
     file.write("datastore.thrift.threads = {} \n".format(options.data_store_thrift_threads))
 
-    file.write("node.monitor.thrift.ports = {} \n".format(options.node_monitor_ports))
     file.write("node.monitor.thrift.threads = {} \n".format(options.node_monitor_thrift_threads))
 
-    file.write("node.enqueue.thrift.ports = {} \n".format(options.node_enqueue_ports))
     file.write("node.enqueue.thrift.threads = {} \n".format(options.node_enqueue_thrift_threads))
     file.write("replay.with.delay = {} \n".format(options.replay_with_delay))
     file.write("replay.with.disk = {} \n".format(options.replay_with_disk))
