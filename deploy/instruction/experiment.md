00f022d309b72639a2e6d6557d583c325b8fb43f
Wei Da <wd312@cl.cam.ac.uk>
2025-07-27 19:52:14 +0100

    fix the setup scripts and add the new exp instruction md

---
+++ deploy/instruction/experiment.md
@@ -0,0 +1,7 @@
+All of the testing scripts has been well packed in the `deploy/python/scripts/` directory with end-to-end shell script to trigger easily with 1-click.
+
+After generate the host configuration and hostname list by following configuration-generation.md
+To set up the testing cluster, just run the following command:
+```bash
+cd dodoor && sh deploy/script/setup.sh
+```
--- deploy/script/setup.sh
+++ deploy/script/setup.sh
@@ -1,4 +1,3 @@
 parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host  "sudo rm -rf dodoor"
 parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host  "git clone https://github.com/AKafakA/dodoor.git"
-parallel-ssh -t 0 -h deploy/resources/host_addresses/cloud_lab/test_host  "cd dodoor && git checkout main && git pull && sh rebuild.sh"
-parallel-ssh -h deploy/resources/host_addresses/cloud_lab/test_host "cd dodoor && sh setup_docker.sh"
+parallel-ssh -t 0 -h deploy/resources/host_addresses/cloud_lab/test_host  "cd dodoor && git checkout main && git pull && sh rebuild.sh && sh setup_docker.sh"
